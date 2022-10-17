c45d0ad94c2102cc64b3bbdc78cef31215070ef1
Tino Sabetta <tino.sabetta@gmail.com>
2022-10-17 15:49:43 +0200

    created shuffle and deal of cards

--- js/app.js
+++ js/app.js
@@ -2,10 +2,34 @@
 Imports
 ---------------------------------------------------------------------------------------------------*/
 
+import {
+    justOneWords
+}
+from './words.js';
+
 /* --------------------------------------------------------------------------------------------------
 Variables
 ---------------------------------------------------------------------------------------------------*/
+Array.prototype.shuffle = function() {
+    var i = this.length;
+    while (i) {
+        var j = Math.floor(Math.random() * i);
+        var t = this[--i];
+        this[i] = this[j];
+        this[j] = t;
+    }
+    return this;
+};
 
+function loadWords() {
+    const listElements = document.querySelectorAll("li");
+    justOneWords.shuffle();
+
+    for (const li of listElements) {
+        li.textContent = justOneWords[0];
+        justOneWords.push(justOneWords.shift());
+    }
+}
 
 /* --------------------------------------------------------------------------------------------------
 functions
@@ -13,6 +37,7 @@ functions
 
 function init() {
     document.addEventListener("touchstart", function() {}, false);
+    loadWords();
 }
 
 /* --------------------------------------------------------------------------------------------------
---
+++ js/words.js
@@ -0,0 +1,20 @@
+/* --------------------------------------------------------------------------------------------------
+curated Words
+---------------------------------------------------------------------------------------------------*/
+
+export var justOneWords = [
+    "Barbie", "Schokolade", "Schnee", "Krawatte", "Wind",
+    "Gedanke", "Frankenstein", "Strahl", "Akzent", "Dusche",
+    "Marienkäfer", "Senf", "Sherlock", "Flasche", "Virus",
+    "Gift", "Stern", "Wolf", "Lena", "Geburtstag",
+    "Bäcker", "Parfum", "Flamme", "Zombie", "Joker",
+    "Buffy", "Vulkan", "Leidenschaft", "Hahn", "Elektrizität",
+    "Genie", "Dracula", "Löwe", "Socke", "Freitag",
+    "Pirat", "Ferien", "Elfe", "Magnet", "Gabel",
+    "Sirup", "Wald", "Waage", "Zeus", "Kakerlake",
+    "Dinosaurier", "Gelb", "Pilz", "Taube", "Pikachu",
+    "Asterix", "Roboter", "Komödie", "Sprache", "Friseur",
+    "Cocktail", "Mario", "Korken", "Geige", "Pfirsich",
+    "Fuchs", "Polizei", "Simpson", "Donut", "Zeitung",
+    "Berge", "Erdnuss", "Glühbirne", "Jedi", "Chili"
+];
